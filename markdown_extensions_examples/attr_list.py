44a0672dba054b68fd66c7d23421528e5acb6222
boomxy <2794865850@qq.com>
2025-05-06 13:05:48 +0800

    docs(markdown_extensions_examples): 更新属性列表扩展示例
    -增加了对属性列表扩展的详细说明和示例
    - 添加了 HTML 结构和样式，更好地展示了属性列表的效果
    - 优化了代码结构，提高了可读性和可维护性

--- markdown_extensions_examples/attr_list.py
+++ markdown_extensions_examples/attr_list.py
@@ -5,13 +5,47 @@ import markdown
 md = markdown.Markdown(extensions=["markdown.extensions.attr_list"])
 
 # 示例 Markdown 文本
-sample_text = """# 示例标题
+sample_text = """# 属性列表扩展
 
-这是一个使用 attr_list 扩展的示例。
+这是一个使用 attr_list 属性列表扩展的示例。
+
+这是带有 id = "an_id" 的块
+{: #an_id }
+
+这是带有 class="a_class" 的块
+{: .a_class }
+
+要定义块级元素的属性，属性列表应在块的最后一行本身定义。
+这是一个带有 id = "an_id" 和 class="a_class" 属性的块
+{: #an_id .a_class }
+
+这个新的块什么都没有，是默认的样式
 """
 
 # 转换 Markdown 文本
-html = md.convert(sample_text)
+convert_html = md.convert(sample_text)
+
+html = f"""
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>属性列表扩展</title>
+    <style>
+        #an_id {{
+            color: blue;
+        }}
+        
+        .a_class {{
+            background-color: lightgreen;
+        }}
+    </style>
+</head>
+<body>
+{convert_html}
+</body>
+</html>
+"""
 
 # 输出结果
 print(html)
