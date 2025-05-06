c81154b93a77925ff3eea8c044c45a855d0d5319
boomxy <2794865850@qq.com>
2025-05-06 13:20:56 +0800

    docs(markdown_extensions_examples): 更新属性列表扩展示例
    - 添加标题属性列表示例
    - 新增内联元素属性列表说明
    - 增加超链接属性列表示例
    - 更新文本样式示例

--- markdown_extensions_examples/attr_list.py
+++ markdown_extensions_examples/attr_list.py
@@ -20,6 +20,19 @@ sample_text = """# 属性列表扩展
 {: #an_id .a_class }
 
 这个新的块什么都没有，是默认的样式
+
+## 标题是例外，允许写在一行上 {: .a_class } 
+
+---
+
+## 下边是内联元素，以超链接为例子
+
+超链接也是可以写在一行上，要在内联元素上定义属性，应在内联元素之后立即定义属性列表，不得有空白。
+
+[attr_list inline](https://python-markdown.github.io/extensions/attr_list/#inline){: class="a_class" title="这是标题!" }
+
+这是有内联元素-> *内联元素*{: class="a_class" } <-且有样式的一行文字。
+
 """
 
 # 转换 Markdown 文本
