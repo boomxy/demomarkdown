5fab34ca064997396e90f6286bf098591b32f636
boomxy <2794865850@qq.com>
2025-05-07 14:22:41 +0800

    docs(markdown_extensions_examples): 更新 nl2br 扩展示例
    - 丰富了示例文本，更好地展示了 nl2br 扩展的功能
    - 添加了对 nl2br 扩展工作原理的解释说明
    - 优化了代码格式，提高了可读性

--- markdown_extensions_examples/nl2br.py
+++ markdown_extensions_examples/nl2br.py
@@ -5,9 +5,21 @@ import markdown
 md = markdown.Markdown(extensions=["markdown.extensions.nl2br"])
 
 # 示例 Markdown 文本
-sample_text = """# 示例标题
+sample_text = """# 强制换行扩展
 
-这是一个使用 nl2br 扩展的示例。
+这是一个使用 nl2br 强制换行扩展的示例。
+
+以前，
+这种紧挨着的两行会被渲染到一行上，忽略换行
+
+现在，换行不
+被忽略，会
+根据文章内容加上换行标签
+
+    会被转换成<br/>
+
+空一行就新的 段落 p 
+
 """
 
 # 转换 Markdown 文本
