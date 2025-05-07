7aba5edc228dc4131c77c77d58a46328fe3d346f
boomxy <2794865850@qq.com>
2025-05-07 14:36:32 +0800

    docs(markdown_extensions_examples): 更新 smarty.py 示例代码
    - 修改示例 Markdown 文本，增加对智能类型扩展的详细说明
    - 添加智能类型扩展转换的 ASCII 码和对应 HTML 实体列表
    -增加使用参考链接

--- markdown_extensions_examples/smarty.py
+++ markdown_extensions_examples/smarty.py
@@ -5,9 +5,19 @@ import markdown
 md = markdown.Markdown(extensions=["markdown.extensions.smarty"])
 
 # 示例 Markdown 文本
-sample_text = """# 示例标题
+sample_text = """# 智能类型扩展
 
-这是一个使用 smarty 扩展的示例。
+这是一个使用 smarty 智能类型扩展的示例。
+
+智能类型扩展会转换 ASCII 码中的
+
+    '  "  << >>  ...  --  ---
+
+这些类型 转换为 html 对应的等效 HTML 实体:
+ 
+ &lsquo;&rsquo; &nbsp;&nbsp; &ldquo; &rdquo;  &nbsp;&nbsp; &laquo; &raquo;  &nbsp;&nbsp; &hellip;  &nbsp;&nbsp; &ndash;  &nbsp;&nbsp; &mdash;
+
+[使用参考](https://python-markdown.github.io/extensions/smarty/#usage)
 """
 
 # 转换 Markdown 文本
