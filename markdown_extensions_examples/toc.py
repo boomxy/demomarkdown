0ef70a1171ddaf9e64a8a5129074e4e290ed2be5
boomxy <2794865850@qq.com>
2025-05-07 14:45:45 +0800

    docs(markdown_extensions_examples): 更新目录扩展示例
    - 增加了更多示例内容，展示了目录扩展的使用效果
    - 添加了目录扩展的使用说明和示例链接
    - 在示例文本中加入了 [TOC] 标记，用于生成目录

--- markdown_extensions_examples/toc.py
+++ markdown_extensions_examples/toc.py
@@ -5,9 +5,55 @@ import markdown
 md = markdown.Markdown(extensions=["markdown.extensions.toc"])
 
 # 示例 Markdown 文本
-sample_text = """# 示例标题
+sample_text = """# 目录扩展
 
-这是一个使用 toc 扩展的示例。
+这是一个使用 toc 目录扩展的示例。
+
+[使用参考](https://python-markdown.github.io/extensions/toc/#usage)
+
+下边是内容，目录扩展生效位置在下边
+
+内容。。。
+内容。。。。
+
+内容。
+内容。。。
+
+# 一级目录 1
+内容。。。
+内容。。。。
+
+## 二级目录 1.1
+内容。。。
+
+
+## 二级目录 1.2
+
+内容。。。
+
+## 二级目录 1.3
+
+内容。。。
+
+# 一级目录 2
+
+内容。。。
+
+## 二级目录 2.1
+
+内容。。。
+
+## 二级目录 2.2
+
+内容。。。
+
+
+---
+
+这里放目录：
+
+[TOC]
+
 """
 
 # 转换 Markdown 文本
