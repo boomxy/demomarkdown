34a5ed2056bd3bf1c306b67e23d79f4e0a16b3b1
boomxy <2794865850@qq.com>
2025-05-07 12:56:50 +0800

    docs(markdown_extensions_examples): 更新 legacy_attrs 扩展示例
    - 增加了对 legacy_attrs 扩展使用方法的详细说明
    - 添加了更多示例，包括标题、文本和图片的属性设置
    - 完善了 HTML 结构，添加了样式定义- 更新了变量名，提高了代码可读性

--- markdown_extensions_examples/legacy_attrs.py
+++ markdown_extensions_examples/legacy_attrs.py
@@ -5,13 +5,53 @@ import markdown
 md = markdown.Markdown(extensions=["markdown.extensions.legacy_attrs"])
 
 # 示例 Markdown 文本
-sample_text = """# 示例标题
+sample_text = """# 遗留/旧属性
 
-这是一个使用 legacy_attrs 扩展的示例。
+这是一个使用 legacy_attrs 遗留/旧属性扩展的示例。
+
+使用方式是
+
+    {@key=value}
+
+一句带有属性 {@class=foo} 定义在任意位置的，带有 class 属性, color: red;
+
+一些 *增强{@id=bar}color: blue;* 带有 id 属性
+
+![图片来源于 bing 壁纸（color: green）{@id=baz}](https://cn.bing.com/)
+![图片来源于 bing 壁纸（color: green）{@id=baz}](https://cn.bing.com/th?id=OHR.DunluceIreland_ZH-CN2412229757_UHD.jpg&pid=hp&w=1920)
+
 """
 
 # 转换 Markdown 文本
-html = md.convert(sample_text)
+convert_html = md.convert(sample_text)
+
+html = f"""
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Markdown Extensions Example</title>
+    <style>
+     .foo {{
+        color: red;
+     }}
+     
+     #bar {{
+         color: blue;
+     }}
+     
+     #baz {{
+        color: green; 
+    }}
+    </style>
+</head>
+<body>
+    <div class="markdown-body">
+       {convert_html}
+    </div>
+</body>
+</html>
+"""
 
 # 输出结果
 print(html)
