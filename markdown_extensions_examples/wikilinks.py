0b664ecbd22470450a32441f20040f70ff6a5305
boomxy <2794865850@qq.com>
2025-05-07 14:49:55 +0800

    docs(wikilinks): 更新维基链接扩展示例
    - 修改文档标题为"维基链接扩展"
    - 增加对 wikilinks 扩展功能的详细说明
    - 添加示例链接和使用说明

--- markdown_extensions_examples/wikilinks.py
+++ markdown_extensions_examples/wikilinks.py
@@ -5,9 +5,15 @@ import markdown
 md = markdown.Markdown(extensions=["markdown.extensions.wikilinks"])
 
 # 示例 Markdown 文本
-sample_text = """# 示例标题
+sample_text = """# 维基链接扩展
 
-这是一个使用 wikilinks 扩展的示例。
+这是一个使用 wikilinks 维基链接扩展的示例。
+
+    任何[[括号]]单词都会转换为链接
+
+[[维基]]
+
+一般情况下会和 [meta](/meta) 联合起来[使用](https://python-markdown.github.io/extensions/wikilinks/#using-with-meta-data-extension)
 """
 
 # 转换 Markdown 文本
