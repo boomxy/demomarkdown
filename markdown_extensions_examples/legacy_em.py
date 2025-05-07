58883ad1b1d474255d40f32cb34aa563a504f30b
boomxy <2794865850@qq.com>
2025-05-07 13:03:50 +0800

    docs(markdown_extensions_examples): 更新 legacy_em.py 示例代码
    - 添加旧的倾斜扩展示例
    -增加 HTML 样式以突出显示效果
    - 优化代码结构和注释

--- markdown_extensions_examples/legacy_em.py
+++ markdown_extensions_examples/legacy_em.py
@@ -5,13 +5,35 @@ import markdown
 md = markdown.Markdown(extensions=["markdown.extensions.legacy_em"])
 
 # 示例 Markdown 文本
-sample_text = """# 示例标题
+sample_text = """# 旧的倾斜扩展
 
-这是一个使用 legacy_em 扩展的示例。
+这是一个使用 legacy_em 传统/旧倾斜扩展的示例。
+
+    原本的样子是 _连_接_
+
+当使用下划线的时候 _连_接_ 这样的文字 会被替换
+
+
+
+或者期望的效果 *连_接* 这样
 """
 
 # 转换 Markdown 文本
-html = md.convert(sample_text)
+convert_html = md.convert(sample_text)
+
+html = f"""<html>
+<head>
+<style>
+em {{
+   color: red;
+   font-size:2em;
+}}
+</style>
+</head>
+<body>
+{convert_html}
+</body>
+</html>"""
 
 # 输出结果
 print(html)
