00159a31dd2648dd650c3310526b584f70a0ead0
boomxy <2794865850@qq.com>
2025-05-08 10:15:59 +0800

    docs(markdown_extensions_examples): 更新 extra 扩展示例
    - 添加了更多 extra 扩展的使用示例，包括脚注、代码块、属性列表、定义列表、表格、缩写和 Markdown in HTML
    - 详细说明了 extra扩展包含的所有子扩展
    - 调整了示例文本，使其更具说明性

--- markdown_extensions_examples/extra.py
+++ markdown_extensions_examples/extra.py
@@ -1,17 +1,67 @@
 import markdown
+
+from markdown.extensions import extra
 # 示例脚本: extra
 
 # 初始化 Markdown 实例，启用 abbr 扩展
 md = markdown.Markdown(extensions=["markdown.extensions.extra"])
 
 # 示例 Markdown 文本
-sample_text = """# 示例标题
+sample_text = """# 标准扩展
 
-这是一个使用 extra123 扩展的示例。
+这是一个使用 extra 标准扩展的示例。
+
+如果开启了使用了这个 extra 标准扩展，相当于一次性启用了如下扩展:
+
+- [fenced_code](/fenced_code)
+- [footnotes](/footnotes)
+- [attr_list](/attr_list)
+- [def_list](/def_list)
+- [tables](/tables)
+- [abbr](/abbr)
+- [md_in_html](/abbr)
+
+这是 footnotes[^1] 示例:
+
+这是 fenced_code 示例:
+
+```
+这是一行fenced_code代码
+```
+
+这是一个 attr_list 示例{class="a_class"}, 在生成的元素里会有出现
+
+这是 def_list 示例：
+
+苹果🍎
+:   苹果属植物的果类果实
+    迷迭香科。
+:   新的苹果品种
+
+这是 tables 示例：
+
+表头   |  表头2
+----- | -----
+ 内容  | 内容
+ 内容  | 内容
+
+这是 abbr 示例
+
+这个 ABBR 是缩写的。
+*[ABBR]:  缩写的意思
+
+这是 md_in_html 示例:
+
+<div markdown="1" style="color:red">*倾斜字体* markdown 写法</div>
+
+
+[^1]: 开启了此扩展
+
 """
 
 # 转换 Markdown 文本
 html = md.convert(sample_text)
 
+print("开启了这些扩展", extra.extensions)
 # 输出结果
 print(html)
